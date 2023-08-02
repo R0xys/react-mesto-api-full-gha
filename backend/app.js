72785ccc3fb1b3bb731347c7013292fb7efa80b2
r0_xyz <maxhard243@gmail.com>
2023-08-02 17:17:21 +0300

    refactor: fix cors request

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+const corsOptions = {
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'https://r0-0ky.nomoreparties.co',
+  ],
+  credentials: true,
+};
+
 mongoose.connect('mongodb://127.0.0.1/mestodb');
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use(requestLogger);
-app.use(require('./middlewares/corsRequest'));
 app.post('/signin', require('./requestValidation').loginBodyValidator, require('./controllers/login').login);
 app.post('/signup', require('./requestValidation').createUserBodyValidator, require('./controllers/users').createUser);
 
