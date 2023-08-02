95dc8c3373c71a7337d194e23d8634bf16293625
r0_xyz <maxhard243@gmail.com>
2023-08-02 18:50:53 +0300

    fix: add secure cookie atribute

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -7,7 +7,12 @@ module.exports.login = (req, res, next) => {
   User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, jwtKey, { expiresIn: '7d' });
-      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true, sameSite: 'None' });
+      res.cookie('jwt', token, {
+        maxAge: 3600000 * 24 * 7,
+        httpOnly: true,
+        sameSite: 'None',
+        secure: true,
+      });
       res.send({
         _id: user._id,
         name: user.name,
