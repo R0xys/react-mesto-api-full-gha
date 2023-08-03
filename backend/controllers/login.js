e2db0dcba222488bc08d01e616ba51706918d19d
r0_xyz <maxhard243@gmail.com>
2023-08-03 12:04:28 +0300

    feat: add env variables

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -1,12 +1,11 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-const { jwtKey } = require('../constants');
 
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, jwtKey, { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'imposible-key', { expiresIn: '7d' });
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,6 +1,5 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorizedError');
-const { jwtKey } = require('../constants');
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
@@ -9,7 +8,7 @@ module.exports = (req, res, next) => {
   }
   let payload;
   try {
-    payload = jwt.verify(token, jwtKey);
+    payload = jwt.verify(token, process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'imposible-key');
   } catch (err) {
     return next(new UnauthorizedError('Необходима авторизация'));
   }
