e35b645481b10ab0a34b2c4c98f891ecb503f8e4
r0_xyz <maxhard243@gmail.com>
2023-08-03 22:10:46 +0300

    hotfix

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -11,7 +11,6 @@ module.exports.login = (req, res, next) => {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
         sameSite: 'None',
-        secure: true,
       });
       res.send({
         _id: user._id,
